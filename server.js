65d37681f903f676b09ef8c1051b81aad6c34e81
Van Neves <vaneves@vaneves.com>
2024-01-05 09:40:11 -0300

    feat/list-school cria endpoint de listar escolas

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { PrismaClient } = require('@prisma/client');
 
+const prisma = new PrismaClient();
 const app = express();
 const PORT = 3000;
 
@@ -8,7 +10,15 @@ app.use(bodyParser.json());
 
 app.get('/ping', (req, res) => {
   res.send('pong');
-})
+});
+
+app.get('/schools', async (req, res) => {
+  const schools = await prisma
+    .school
+    .findMany();
+
+  res.json(schools);
+});
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
